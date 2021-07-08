aefd42db90695aec374df81e0c2d221e57b1aa03
David Liu <david.yx.liu@oracle.com>
2021-07-07 22:11:22 -0400

    Update cli.sh

--- fedora/cli.sh
+++ fedora/cli.sh
@@ -8,7 +8,7 @@ baseurl=https://packages.microsoft.com/yumrepos/azure-cli
 enabled=1
 gpgcheck=1
 gpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/azure-cli.repo
-  sudo dnf install azure-cli
+  sudo dnf install -y azure-cli
 }
 setup(){
   az login
