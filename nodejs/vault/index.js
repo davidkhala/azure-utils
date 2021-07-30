1e13cbdd830898f9571b5e6d716af7c172b57a73
DavidLiu <david.yx.liu@oracle.com>
2021-07-30 16:16:25 +0800

    signature result inconsistent is expected

--- nodejs/vault/index.js
+++ nodejs/vault/index.js
@@ -1,211 +1,228 @@
-const {KeyClient, CryptographyClient} = require("@azure/keyvault-keys");
-const {createHash} = require('crypto')
+const {KeyClient, CryptographyClient} = require('@azure/keyvault-keys');
+const {createHash} = require('crypto');
 /**
  * @enum string
  */
 const KeyType = {
-    EC: 'EC',// Elliptic Curve
-    'EC-HSM': 'EC-HSM',
-    RSA: 'RSA',
-    'RSA-HSM': 'RSA-HSM',
-    oct: 'oct',// Octet sequence (used to represent symmetric keys)
-    'oct-HSM': 'oct-HSM',
-}
+	EC: 'EC',// Elliptic Curve
+	'EC-HSM': 'EC-HSM',
+	RSA: 'RSA',
+	'RSA-HSM': 'RSA-HSM',
+	oct: 'oct',// Octet sequence (used to represent symmetric keys)
+	'oct-HSM': 'oct-HSM',
+};
 
 const KnownSignatureAlgorithms = {
-    /** RSASSA-PSS using SHA-256 and MGF1 with SHA-256, as described in https://tools.ietf.org/html/rfc7518 */
-    PS256: "PS256",
-    /** RSASSA-PSS using SHA-384 and MGF1 with SHA-384, as described in https://tools.ietf.org/html/rfc7518 */
-    PS384: "PS384",
-    /** RSASSA-PSS using SHA-512 and MGF1 with SHA-512, as described in https://tools.ietf.org/html/rfc7518 */
-    PS512: "PS512",
-    /** RSASSA-PKCS1-v1_5 using SHA-256, as described in https://tools.ietf.org/html/rfc7518 */
-    RS256: "RS256",
-    /** RSASSA-PKCS1-v1_5 using SHA-384, as described in https://tools.ietf.org/html/rfc7518 */
-    RS384: "RS384",
-    /** RSASSA-PKCS1-v1_5 using SHA-512, as described in https://tools.ietf.org/html/rfc7518 */
-    RS512: "RS512",
-    /** ECDSA using P-256 and SHA-256, as described in https://tools.ietf.org/html/rfc7518. */
-    ES256: "ES256",
-    /** ECDSA using P-384 and SHA-384, as described in https://tools.ietf.org/html/rfc7518 */
-    ES384: "ES384",
-    /** ECDSA using P-521 and SHA-512, as described in https://tools.ietf.org/html/rfc7518 */
-    ES512: "ES512",
-    /** ECDSA using P-256K and SHA-256, as described in https://tools.ietf.org/html/rfc7518 */
-    ES256K: "ES256K"
-}
+	/** RSASSA-PSS using SHA-256 and MGF1 with SHA-256, as described in https://tools.ietf.org/html/rfc7518 */
+	PS256: 'PS256',
+	/** RSASSA-PSS using SHA-384 and MGF1 with SHA-384, as described in https://tools.ietf.org/html/rfc7518 */
+	PS384: 'PS384',
+	/** RSASSA-PSS using SHA-512 and MGF1 with SHA-512, as described in https://tools.ietf.org/html/rfc7518 */
+	PS512: 'PS512',
+	/** RSASSA-PKCS1-v1_5 using SHA-256, as described in https://tools.ietf.org/html/rfc7518 */
+	RS256: 'RS256',
+	/** RSASSA-PKCS1-v1_5 using SHA-384, as described in https://tools.ietf.org/html/rfc7518 */
+	RS384: 'RS384',
+	/** RSASSA-PKCS1-v1_5 using SHA-512, as described in https://tools.ietf.org/html/rfc7518 */
+	RS512: 'RS512',
+
+
+	/** ECDSA using P-256 and SHA-256, as described in https://tools.ietf.org/html/rfc7518. */
+	ES256: 'ES256',
+	/** ECDSA using P-384 and SHA-384, as described in https://tools.ietf.org/html/rfc7518 */
+	ES384: 'ES384',
+	/** ECDSA using P-521 and SHA-512, as described in https://tools.ietf.org/html/rfc7518 */
+	ES512: 'ES512',
+	/** ECDSA using P-256K and SHA-256, as described in https://tools.ietf.org/html/rfc7518 */
+	ES256K: 'ES256K'
+};
 const SignDataAlgorithms = {
-    PS256: KnownSignatureAlgorithms.PS256,
-    RS256: KnownSignatureAlgorithms.RS256,
-    PS384: KnownSignatureAlgorithms.PS384,
-    RS384: KnownSignatureAlgorithms.RS384,
-    PS512: KnownSignatureAlgorithms.PS512,
-    RS512: KnownSignatureAlgorithms.RS512
-}
+	PS256: KnownSignatureAlgorithms.PS256,
+	RS256: KnownSignatureAlgorithms.RS256,
+	PS384: KnownSignatureAlgorithms.PS384,
+	RS384: KnownSignatureAlgorithms.RS384,
+	PS512: KnownSignatureAlgorithms.PS512,
+	RS512: KnownSignatureAlgorithms.RS512
+};
 
 /**
  *
  * @enum string
  */
 const EncryptionAlgorithm = {
-    'RSA-OAEP': 'RSA-OAEP',
-    'RSA-OAEP-256': 'RSA-OAEP-256',
-    RSA1_5: 'RSA1_5',
-    A128GCM: 'A128GCM',
-    A192GCM: 'A192GCM',
-    A256GCM: 'A256GCM',
-    A128KW: 'A128KW',
-    A192KW: 'A192KW',
-    A256KW: 'A256KW',
-    A128CBC: 'A128CBC',
-    A192CBC: 'A192CBC',
-    A256CBC: 'A256CBC',
-    A128CBCPAD: 'A128CBCPAD',
-    A192CBCPAD: 'A192CBCPAD',
-    A256CBCPAD: 'A256CBCPAD',
-}
+	'RSA-OAEP': 'RSA-OAEP',
+	'RSA-OAEP-256': 'RSA-OAEP-256',
+	RSA1_5: 'RSA1_5',
+	A128GCM: 'A128GCM',
+	A192GCM: 'A192GCM',
+	A256GCM: 'A256GCM',
+	A128KW: 'A128KW',
+	A192KW: 'A192KW',
+	A256KW: 'A256KW',
+	A128CBC: 'A128CBC',
+	A192CBC: 'A192CBC',
+	A256CBC: 'A256CBC',
+	A128CBCPAD: 'A128CBCPAD',
+	A192CBCPAD: 'A192CBCPAD',
+	A256CBCPAD: 'A256CBCPAD',
+};
 
 class Key {
-    /**
-     *
-     * @param keyVaultName
-     * @param credential
-     * @param [logger]
-     */
-    constructor(keyVaultName, credential, logger = console) {
-        const KVUri = "https://" + keyVaultName + ".vault.azure.net";
-        const client = new KeyClient(KVUri, credential);
-        Object.assign(this, {client, logger, credential})
-    }
+	/**
+	 *
+	 * @param keyVaultName
+	 * @param credential
+	 * @param [logger]
+	 */
+	constructor(keyVaultName, credential, logger = console) {
+		const KVUri = 'https://' + keyVaultName + '.vault.azure.net';
+		const client = new KeyClient(KVUri, credential);
+		Object.assign(this, {client, logger, credential});
+	}
 
-    /**
-     *
-     * @param {string} keyName
-     * @param {KeyType} keyType
-     * @return {Promise<void>}
-     */
-    async create(keyName, keyType) {
-        const {client} = this
-        await client.createKey(keyName, keyType);
-    }
+	/**
+	 *
+	 * @param {string} keyName
+	 * @param {KeyType} keyType
+	 * @return {Promise<void>}
+	 */
+	async create(keyName, keyType) {
+		const {client} = this;
+		await client.createKey(keyName, keyType);
+	}
 
-    async asCrypto(keyName) {
-        const key = await this.client.getKey(keyName)
-        return new Cryptography(key, this.credential)
-    }
+	async asCrypto(keyName) {
+		const key = await this.client.getKey(keyName);
+		return new Cryptography(key, this.credential);
+	}
 
 
-    /**
-     *
-     * @param {string} keyName
-     * @return {Promise<KeyVaultKey>}
-     */
-    async get(keyName) {
-        const {client} = this
-        return await client.getKey(keyName);
-    }
+	/**
+	 *
+	 * @param {string} keyName
+	 * @return {Promise<KeyVaultKey>}
+	 */
+	async get(keyName) {
+		const {client} = this;
+		return await client.getKey(keyName);
+	}
 
-    async delete(keyName, {sync, force}) {
-        const {client} = this
-        let key
-        const throwOthers = (e) => {
-            const {name, code, message} = e
-            if (name !== 'RestError' || code !== 'KeyNotFound') {
-                throw e
-            }
-            this.logger.warn(message)
-        }
-        try {
-            const result = await client.getKey(keyName)
-            key = result.key
-            this.logger.info(key)
-        } catch (e) {
-            throwOthers(e)
-        }
+	async delete(keyName, {sync, force}) {
+		const {client} = this;
+		let key;
+		const throwOthers = (e) => {
+			const {name, code, message} = e;
+			if (name !== 'RestError' || code !== 'KeyNotFound') {
+				throw e;
+			}
+			this.logger.warn(message);
+		};
+		try {
+			const result = await client.getKey(keyName);
+			key = result.key;
+			this.logger.info(key);
+		} catch (e) {
+			throwOthers(e);
+		}
 
-        if (key) {
-            const deletePoller = await client.beginDeleteKey(keyName);
-            if (sync) {
-                await deletePoller.pollUntilDone();
-            }
-        }
+		if (key) {
+			const deletePoller = await client.beginDeleteKey(keyName);
+			if (sync) {
+				await deletePoller.pollUntilDone();
+			}
+		}
 
-        if (force) {
-            await client.purgeDeletedKey(keyName);
-        }
-    }
+		if (force) {
+			await client.purgeDeletedKey(keyName);
+		}
+	}
 
 }
 
 const algorithmMap = {
-    RSA: EncryptionAlgorithm["RSA-OAEP-256"],
-    EC: KnownSignatureAlgorithms.ES256,
-}
+	RSA: EncryptionAlgorithm['RSA-OAEP-256'],
+	EC: KnownSignatureAlgorithms.ES256,
+};
 
 class Cryptography {
-    /**
-     *
-     * @param {KeyVaultKey} key
-     * @param credential
-     */
-    constructor(key, credential) {
-        this.client = new CryptographyClient(key, credential)
-        this.key = key
-    }
+	/**
+	 *
+	 * @param {KeyVaultKey} key
+	 * @param credential
+	 */
+	constructor(key, credential) {
+		this.client = new CryptographyClient(key, credential);
+		this.key = key;
+	}
 
-    /**
-     *
-     * @param {string} plaintext
-     * @param {EncryptionAlgorithm} [algorithm]
-     * @return {Promise<Uint8Array>}
-     */
-    async encrypt(plaintext, algorithm) {
-        if (!algorithm) {
-            algorithm = algorithmMap[this.key.keyType]
-        }
-        const {result} = await this.client.encrypt({algorithm, plaintext: Buffer.from(plaintext)})
-        return result
-    }
+	/**
+	 *
+	 * @param {string} plaintext
+	 * @param {EncryptionAlgorithm} [algorithm]
+	 * @return {Promise<Uint8Array>}
+	 */
+	async encrypt(plaintext, algorithm) {
+		if (!algorithm) {
+			algorithm = algorithmMap[this.key.keyType];
+		}
+		const {result} = await this.client.encrypt({algorithm, plaintext: Uint8Array.from(plaintext)});
+		return result;
+	}
 
-    /**
-     *
-     * @param {Uint8Array} ciphertext
-     * @param [algorithm]
-     * @return {Promise<string>}
-     */
-    async decrypt(ciphertext, algorithm) {
-        if (!algorithm) {
-            algorithm = algorithmMap[this.key.keyType]
-        }
-        const {result} = await this.client.decrypt({algorithm, ciphertext})
-        return result.toString()
-    }
+	/**
+	 *
+	 * @param {Uint8Array} ciphertext
+	 * @param [algorithm]
+	 * @return {Promise<string>}
+	 */
+	async decrypt(ciphertext, algorithm) {
+		if (!algorithm) {
+			algorithm = algorithmMap[this.key.keyType];
+		}
+		const {result} = await this.client.decrypt({algorithm, ciphertext});
+		return result.toString();
+	}
 
-    async sign(message, algorithm) {
+	async sign(message, algorithm) {
 
+		if (!algorithm) {
+			algorithm = algorithmMap[this.key.keyType];
+		}
 
-        if (!algorithm) {
-            algorithm = algorithmMap[this.key.keyType]
-            if (this.key.keyType === KeyType.EC) {
-                const hash = createHash("sha256");
+		if (this.key.keyType === KeyType.EC) {
+			const hash = createHash('sha256');
 
-                const digest = hash.update(message).digest();
-                const {result} = await this.client.sign(algorithm, digest)
-                return result.toString()
-            }
+			const digest = hash.update(message).digest();
+			const {result} = await this.client.sign(algorithm, digest);
+			return result;
+		}
+		const {result} = await this.client.signData(algorithm, Uint8Array.from(message));
+		return result;
+	}
 
-        }
-        const {result} = await this.client.signData(algorithm, Buffer.from(message))
-        return Buffer.from(result).toString('hex')
-    }
+	async verify(message, signature, algorithm) {
+		if (!algorithm) {
+			algorithm = algorithmMap[this.key.keyType];
+		}
+		if (this.key.keyType === KeyType.EC) {
+			const hash = createHash('sha256');
+
+			const digest = hash.update(message).digest();
+			const {result} = await this.client.verify(algorithm, digest, signature);
+			return result;
+		}
+
+		const {result} = await this.client.verifyData(algorithm, Uint8Array.from(message), Uint8Array.from(signature));
+		return result;
+	}
 }
 
 module.exports = {
-    Key,
-    KeyType,
-    EncryptionAlgorithm,
-    Cryptography,
-    KnownSignatureAlgorithms,
-    SignDataAlgorithms,
-}
+	Key,
+	KeyType,
+	EncryptionAlgorithm,
+	Cryptography,
+	KnownSignatureAlgorithms,
+	SignDataAlgorithms,
+};
 
--- nodejs/vault/test/keyVault.js
+++ nodejs/vault/test/keyVault.js
@@ -1,64 +1,63 @@
-const {Key, KeyType} = require('../index')
-const {getConfigFileCredential} = require('../../login')
-const vaultName = 'davidkhala-vault'
-const assert = require('assert')
-const configFileCredential = getConfigFileCredential()
+const {Key, KeyType} = require('../index');
+const {getConfigFileCredential} = require('../../login');
+const vaultName = 'davidkhala-vault';
+const assert = require('assert');
+const configFileCredential = getConfigFileCredential();
 
 describe('key: EC', () => {
 
-    const keyName = 'EC'
-    const keyType = KeyType.EC
+	const keyName = 'EC';
+	const keyType = KeyType.EC;
 
-    const keyOpt = new Key(vaultName, configFileCredential)
-    it('create', async function () {
-        this.timeout(40000)
-        await keyOpt.create(keyName, keyType)
-    })
-    it('get', async () => {
-        const keyInfo = await keyOpt.get(keyName)
-        console.log(keyInfo)
-    })
-    it('sign', async function () {
-        this.timeout(0)
+	const keyOpt = new Key(vaultName, configFileCredential);
+	it('create', async function () {
+		this.timeout(40000);
+		await keyOpt.create(keyName, keyType);
+	});
+	it('get', async () => {
+		const keyInfo = await keyOpt.get(keyName);
+		console.log(keyInfo);
+	});
+	it('sign and verify', async function () {
+		this.timeout(0);
 
-        const keyCrypto = await keyOpt.asCrypto(keyName)
-        const message = 'My data'
+		const keyCrypto = await keyOpt.asCrypto(keyName);
+		const message = 'My data';
 
-        const signature = await keyCrypto.sign(message)
-        const expected = 'efbfbd65efbfbd25efbfbdd683efbfbdefbfbd0a7665efbfbd41efbfbd0eefbfbd40efbfbdefbfbd35efbfbdefbfbd7fefbfbd6fefbfbd0fefbfbdefbfbd3defbfbdefbfbd4cefbfbdefbfbd14efbfbdefbfbd6b68efbfbd0f16efbfbd2501efbfbdefbfbd61efbfbd4f34efbfbdefbfbdefbfbdcb9d0cefbfbd0befbfbdefbfbd'
-        // FIXME, why each time we have a different signature result?
-        assert.strictEqual(signature, expected)
-    })
-    it('delete', async function () {
-        this.timeout(400000)
-        await keyOpt.delete(keyName, {sync: true})
-        await keyOpt.delete(keyName, {force: true})
-    })
+		const signature = await keyCrypto.sign(message);
+		const isValid = await keyCrypto.verify(message, signature);
+		assert.ok(isValid);
+	});
+	it('delete', async function () {
+		this.timeout(400000);
+		await keyOpt.delete(keyName, {sync: true});
+		await keyOpt.delete(keyName, {force: true});
+	});
 
-})
+});
 describe('key RSA', () => {
-    const keyName = 'RSA'
-    const keyType = KeyType.RSA
+	const keyName = 'RSA';
+	const keyType = KeyType.RSA;
 
-    const keyOpt = new Key(vaultName, configFileCredential)
-    it('create', async () => {
-        await keyOpt.create(keyName, keyType)
-    })
-    it('get', async () => {
-        const keyInfo = await keyOpt.get(keyName)
-        console.log(keyInfo)
-    })
-    let cipherResult
-    it('encrypt', async function () {
-        this.timeout(40000)
-        const cryptoOperator = await keyOpt.asCrypto(keyName)
-        cipherResult = await cryptoOperator.encrypt('abc')
+	const keyOpt = new Key(vaultName, configFileCredential);
+	it('create', async () => {
+		await keyOpt.create(keyName, keyType);
+	});
+	it('get', async () => {
+		const keyInfo = await keyOpt.get(keyName);
+		console.log(keyInfo);
+	});
+	let cipherResult;
+	it('encrypt', async function () {
+		this.timeout(40000);
+		const cryptoOperator = await keyOpt.asCrypto(keyName);
+		cipherResult = await cryptoOperator.encrypt('abc');
 
-    })
-    it('decrypt', async function () {
-        this.timeout(40000)
-        const cryptoOperator = await keyOpt.asCrypto(keyName)
-        const result = await cryptoOperator.decrypt(cipherResult)
-        console.debug(result)
-    })
-})
+	});
+	it('decrypt', async function () {
+		this.timeout(40000);
+		const cryptoOperator = await keyOpt.asCrypto(keyName);
+		const result = await cryptoOperator.decrypt(cipherResult);
+		console.debug(result);
+	});
+});
