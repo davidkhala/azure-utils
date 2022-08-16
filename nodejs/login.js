fee123c633d630c1b2b44ac76171897f74e35cc1
davidliu <david-khala@hotmail.com>
2022-08-16 11:11:04 +0800

    back before test auth

--- nodejs/login.js
+++ nodejs/login.js
@@ -1,3 +1,9 @@
 import {DefaultAzureCredential} from '@azure/identity';
+import {SubscriptionClient} from '@azure/arm-subscriptions';
 
 export const getConfigFileCredential = () => new DefaultAzureCredential();
+
+export const auth = (credential) => {
+	const client = new SubscriptionClient(credential);
+	client.subscriptions.list();
+};
---
+++ nodejs/test/auth.js
@@ -0,0 +1,18 @@
+import {getConfigFileCredential} from '../login.js';
+import {SubscriptionClient} from '@azure/arm-subscriptions';
+
+const credentials = getConfigFileCredential();
+const client = new SubscriptionClient(credentials);
+describe('auth', function () {
+	this.timeout(0);
+	it('subscriptions.list', async () => {
+
+		const list = client.subscriptions.list();
+		const values = [];
+		for await (const value of list) {
+			values.push(value);
+		}
+		console.debug(values);
+
+	});
+});
