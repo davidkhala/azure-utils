9a1b34fec26ce10511588a7148682ce58387f748
David Liu <david-khala@hotmail.com>
2023-03-15 10:04:43 +0800

    Update sshkey.sh

--- cli/sshkey.sh
+++ cli/sshkey.sh
@@ -1,6 +1,7 @@
 create(){
   local name=$1
-  az sshkey create --name $name 2>&1 | sed -n '2 p'
+  az sshkey create --name $name 2>&1 | sed -n '2 p' | awk -F、" '{print $2}'
+  
 }
 delete(){
   local name=$1
