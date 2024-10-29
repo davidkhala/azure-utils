5bc81bfdb005a7de5595843ae8c19ece193ed34a
David Liu <david-khala@hotmail.com>
2024-10-29 17:49:21 +0800

    add delete

--- cli/entra.sh
+++ cli/entra.sh
@@ -1,10 +1,14 @@
 create-service-principal() {
-  local scopes="/subscriptions/$subscription"
-  if [[ -n $rg ]]; then
-    scopes="$scopes/resourceGroups/$rg"
-  fi
+    local scopes="/subscriptions/$subscription"
+    if [[ -n $rg ]]; then
+        scopes="$scopes/resourceGroups/$rg"
+    fi
 
-  az ad sp create-for-rbac --name $1 --role Contributor --scopes $scopes --query "{appId:appId, password:password}"
+    az ad sp create-for-rbac --name $1 --role Contributor --scopes $scopes --query "{appId:appId, password:password}"
 
 }
+delete() {
+    az ad sp delete --id $1
+}
+
 $@
