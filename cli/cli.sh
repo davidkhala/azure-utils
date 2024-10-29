4b9f735b252bb18de6ab21f771438b8ea565f057
David Liu <david-khala@hotmail.com>
2024-10-29 16:17:11 +0800

    generic service principal create

--- cli/cli.sh
+++ cli/cli.sh
@@ -4,10 +4,15 @@ setup() {
 ping() {
   az account list
 }
-create-service-principal(){
-  az ad sp create-for-rbac --name $1 --role Contributor --scopes /subscriptions/d02180af-0630-4747-ab1b-0d3b3c12dafb
+create-service-principal() {
+  local scopes="/subscriptions/$subscription"
+  if [[ -n $rg ]]; then
+    scopes="$scopes/resourceGroups/$rg"
+  fi
+
+  az ad sp create-for-rbac --name $1 --role Contributor --scopes $scopes
 }
-regions(){
+regions() {
   az account list-locations --query "[].{DisplayName:displayName, Name:name}"
 }
 
--- cli/davidkhala.sh
+++ cli/davidkhala.sh
@@ -1,2 +1,4 @@
 az config set defaults.location=eastasia
 az config set defaults.group=root-compartment
+
+az account set --subscription d02180af-0630-4747-ab1b-0d3b3c12dafb
