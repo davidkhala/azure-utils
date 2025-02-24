a425808a7ba4a481af48597ccdd0f92a59a77a74
David Liu <david-khala@hotmail.com>
2025-02-24 10:54:41 +0800

    standardize enable.run - move purview cli to davidkhala/microsoft

--- data/bricks/cli/bash.sh
+++ data/bricks/cli/bash.sh
@@ -1,6 +1,4 @@
 set -e
-echo y | az databricks -h >/dev/null
-
 show() {
     local ADB_WS_NAME=$1 # Azure Databricks workspace name
     az databricks workspace show --resource-group $rg --name $ADB_WS_NAME $@
@@ -11,7 +9,7 @@ create() {
     sleep 60
 }
 get-access-token() {
-    az account get-access-token --resource 2ff814a6-3304-4ab8-85cb-cd0e6f879c1d -o tsv --query accessToken $@
+    az account get-access-token --resource 2ff814a6-3304-4ab8-85cb-cd0e6f879c1d -o tsv --query accessToken "$@"
 }
 login() {
     local workspace_name=${1:-$workspace_name}
