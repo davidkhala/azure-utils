b2d65b2e1df98a973930b018e028cb27417c3b2b
David Liu <david-khala@hotmail.com>
2025-07-18 21:24:07 +0800

    ugly azure extension

---
+++ py/azext_davidkhala/__init__.py
@@ -0,0 +1,20 @@
+# TODO ugly azure extension
+from azure.cli.core import AzCommandsLoader
+
+
+def say_hello():
+    print("Hello from your custom Azure CLI extension!")
+
+class CommandsLoader(AzCommandsLoader):
+    def __init__(self, cli_ctx=None):
+        from azure.cli.core.commands import CliCommandType
+
+        super().__init__(cli_ctx=cli_ctx,custom_command_type=CliCommandType(operations_tmpl='azext_davidkhala#{}'))
+
+    def load_command_table(self, args):
+        with self.command_group('hello') as g:
+            g.custom_command('world', 'say_hello')
+        return self.command_table
+
+
+COMMAND_LOADER_CLS = MyExtensionCommandsLoader
